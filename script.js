44c0ced5b1e2d46641150a2554c65650cee26e2d
katherine <pv.katherine7@gmail.com>
2022-11-23 16:11:54 +0000

    completed part 1 of challenge

--- script.js
+++ script.js
@@ -9,20 +9,23 @@ const renderPokemonCards = (pokemonArray) => {
 
   pokemonArray.forEach((element) => {
     htmlString += `
-        <section>
-            <img src=${element.sprite}>
-            <h2>${
-              element.name.charAt(0).toUpperCase() + element.name.slice(1)
-            }</h2>
-            <p>${
-              element.name.charAt(0).toUpperCase() + element.name.slice(1)
-            } (#${element.id}) is a ${element.types.join(
+        <section class="card-container">
+        <div class="card">
+            <img src=${element.sprite} alt="Pokemon" card="card__image">
+            <div class="card__content" >
+                <h2 class="card__heading">${
+                  element.name.charAt(0).toUpperCase() + element.name.slice(1)
+                }</h2>
+                <p class="card__text">${
+                  element.name.charAt(0).toUpperCase() + element.name.slice(1)
+                } (#${element.id}) is a ${element.types.join(
       " & "
     )} type pokemon.</p>
-        </section>
+            </div>
+            </div>
+    </section>
         `;
   });
   cardContainer.innerHTML = htmlString;
 };
-
 renderPokemonCards(pokemonArray);
